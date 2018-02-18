a05d2df7173eeb18ce5bfba8650715d0c0f04abb
Konstantin Antonnikov <diretree@gmail.com>
2018-02-18 19:02:13 +0400

    rev. 0003

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,6 +1,12 @@
+import javax.swing.*;
+
 public class MainClass {
 public static void main(String args[]) {
-    System.out.println("Java is cool!");
-    System.out.println("Hello world!");
+    String userName;
+    String birthDate;
+    userName = JOptionPane.showInputDialog("Please enter your name:");
+    JOptionPane.showMessageDialog(null, "Hello, "+userName+"!");
+    birthDate = JOptionPane.showInputDialog("Please enter you birth date:");
+    JOptionPane.showMessageDialog(null, "Thats nice, here is your birth date: \n"+birthDate+"");
 }
 }
